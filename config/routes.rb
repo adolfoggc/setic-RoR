20d92e3b9cb413054d5f7a3fb4cbdb08d766d3de
Carlos Adolfo <adolfo.ggc@gmail.com>
2017-11-10 00:56:29 -0300

    new root

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :members
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'members#index'
 end
